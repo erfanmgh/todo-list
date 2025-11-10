6c3386ee13fab79a06f669f8b628dff18a2e77f5
erfan <1383mgh556@gmail.com>
2025-11-10 03:57:42 +0330

    Add files via upload

---
+++ app/config/connection.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql');
+let connection;
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'root',
+        database: 'todos_db'
+    
+    });
+}
+
+connection.on('connect', () => {
+    console.log(`--> Connected to database '${connection.config.database}'.`);
+});
+
+connection.on('error', err => {
+    console.log('--> Connection error: ', err);
+});
+
+connection.connect();
+
+module.exports = connection;
